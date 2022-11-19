23c83d0bd1b1561099feb1b24b6d2ffbb5eb6da4
SupunArunoda <supunarunoda@gmail.com>
2022-11-19 22:37:10 +0900

    Updated the code to create triage from vertices

--- OpenGLCourseApp/triangle_vertex/main.cpp
+++ OpenGLCourseApp/triangle_vertex/main.cpp
@@ -18,7 +18,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.9f, 1.0f);\n"
 		"}\n\0";
 
 void processInput(GLFWwindow* window) 
@@ -120,6 +120,30 @@ int main()
 	glDeleteShader(vertexShder);
 	glDeleteShader(fragmentShader);
 
+	//create vertex data and buffers
+	float vertices[] = {
+		-0.5f,-0.5f,0.0f,
+		0.5f,-0.5f,0.0f,
+		0.0f,0.5f,0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Unbind the buffer after enable the vertex attribute
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//Unbind the vertex array
+	glBindVertexArray(0);
 
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
@@ -129,11 +153,22 @@ int main()
 		processInput(mainWindow);
 
 		// Clear window
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//draw triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(mainWindow);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwTerminate();
 
 	return 0;
 }
